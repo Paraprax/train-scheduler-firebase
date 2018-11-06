242dc431851657c4d7b5b0c5d5ba77f3c8aa6346
Paraprax <parkerjohnston28@gmail.com>
2018-11-06 03:38:37 -0500

    initialized firebase && var for train object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,23 @@
 $(document).ready(function(){
+
+     // initialization of firebase
+     var config = {
+        apiKey: "AIzaSyD4xVLwGmj5nkSvQUd-jrQEI9_sJ5EjAuA",
+        authDomain: "function-junction-train-app.firebaseapp.com",
+        databaseURL: "https://function-junction-train-app.firebaseio.com/",
+        storageBucket: "function-junction-train-app.appspot.com"
+      };
+  
+    firebase.initializeApp(config);
+  
+    var database = firebase.database();
+  
+    var train = { 
+            name: "",
+            destination: "",
+            firstTime: 0,
+            frequency: 0,
+    }
     
     
 
